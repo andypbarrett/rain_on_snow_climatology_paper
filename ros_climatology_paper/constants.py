2f2e7e74dd94b433349802845fa9e106bd70dc6a
Andy Barrett <apbarret@nsidc.org>
2025-07-14 17:03:56 -0600

    add list of stations with more than 70% data

---
+++ ros_climatology_paper/constants.py
@@ -0,0 +1,19 @@
+
+# Station-ids for stations with T2m records that start on or before 1979-01-01
+# and have more than 70% valid data
+LONG_TERM_STATIONS = ['PANC', 'PABR', 'PABE', 'PABT', 'PACV', 'PASC', 'PAEI', 'PAFA',
+                      'PFYU', 'PABI', 'PAGA', 'PAGK', 'PAIM', 'PAEN', 'PAKN', 'PADQ',
+                      'PAOT', 'PAMC', 'PAMH', 'PANN', 'PAOM', 'PAOR', 'PAAQ', 'PPIZ',
+                      'PARY', 'PASV', 'PATK', 'PATA', 'PAVD', 'PAFB', 'PAYA', 'CYBK',
+                      'CYCB', 'CYCY', 'CYZS', 'CWEU', 'CYUX', 'CYFB', 'CYUT', 'CYRB',
+                      'CYKD', 'CZFM', 'CYFS', 'CYSM', 'CYHY', 'CYEV', 'CYCO', 'CYVQ',
+                      'CYSY', 'CYUB', 'CYZF', 'CYDA', 'CYMA', 'CYOC', 'CYZW', 'CYXY',
+                      'CYVP', 'CYYR', 'EFHA', 'EFHK', 'EFIV', 'EFJY', 'EFKI', 'EFKE',
+                      'EFKU', 'EFKS', 'EFLP', 'EFMA', 'EFMI', 'EFOU', 'EFPO', 'EFRO',
+                      'EFTU', 'EFUT', 'EFVA', 'BGBW', 'BGSF', 'BGTL', 'BIAR', 'BIKF',
+                      'BIRK', 'ENAL', 'ENAT', 'ENDU', 'ENBR', 'ENBV', 'ENBO', 'ENBN',
+                      'ENEV', 'ENKB', 'ENNA', 'ENMH', 'ENML', 'ENOL', 'ENSK', 'ENSB',
+                      'ENTC', 'ENVA', 'UHMA', 'ULAA', 'USHH', 'UHMM', 'ULMM', 'USNN',
+                      'ULPB', 'UHMD', 'USRR', 'UUYY', 'ESNQ', 'ESNK', 'ESPA', 'ESSA',
+                      'ESNN', 'ESNU', 'ESOW', 'ESPE']
+
